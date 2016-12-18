2b28a7be3e859517b88775a98f7f9856f5fcb64a
lin <lin@ubuntu.ubuntu-domain>
2016-12-18 22:48:56 +0800

    add lianjia spider v1

---
+++ py/spider/lianjia/lianjia.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import urllib2  
+import sqlite3
+import time    
+from bs4 import BeautifulSoup  
+time.clock()  
+for i in range(101):  
+    url = 'http://bj.lianjia.com/ershoufang/pg'+str(i)+'/'  
+    page = urllib2.urlopen(url)  
+    soup = BeautifulSoup(page)  
+    for link in soup.find_all('div','houseInfo'):  
+        context = link.get_text()  
+        print(context)  
+print(time.clock())  
