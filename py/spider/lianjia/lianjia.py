d95074d94c1c5fd5ad29d7b9b0f30c93e33d4a15
lin <lin@ubuntu.ubuntu-domain>
2016-12-18 23:31:56 +0800

    lianjia spider v2

--- py/spider/lianjia/lianjia.py
+++ py/spider/lianjia/lianjia.py
@@ -1,15 +1,170 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: 冰蓝
+@site: http://lanbing510.info
+"""
 
+import re
 import urllib2  
 import sqlite3
-import time    
-from bs4 import BeautifulSoup  
-time.clock()  
-for i in range(101):  
-    url = 'http://bj.lianjia.com/ershoufang/pg'+str(i)+'/'  
-    page = urllib2.urlopen(url)  
-    soup = BeautifulSoup(page)  
-    for link in soup.find_all('div','houseInfo'):  
-        context = link.get_text()  
-        print(context)  
-print(time.clock())  
+import random
+import threading
+from bs4 import BeautifulSoup
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+#登录，不登录不能爬取三个月之内的数据,代码参考博客[1]
+#import LianJiaLogIn
+
+#Some User Agents
+hds=[{'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'},\
+     {'User-Agent':'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},\
+     {'User-Agent':'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'},\
+     {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'},\
+     {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/44.0.2403.89 Chrome/44.0.2403.89 Safari/537.36'},\
+     {'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'},\
+     {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'},\
+     {'User-Agent':'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0'},\
+     {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'},\
+     {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'},\
+     {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11'},\
+     {'User-Agent':'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11'},\
+     {'User-Agent':'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11'}]
+
+#北京区域列表
+regions=[u"东城",u"西城",u"朝阳",u"海淀",u"丰台",u"石景山","通州",u"昌平",u"大兴",u"亦庄开发区",u"顺义",u"房山",u"门头沟",u"平谷",u"怀柔",u"密云",u"延庆",u"燕郊"]
+
+
+lock = threading.Lock()
+
+class SQLiteWraper(object):
+    """
+    数据库的一个小封装，更好的处理多线程写入
+    """
+    def __init__(self,path,command='',*args,**kwargs):  
+        self.lock = threading.RLock() #锁  
+        self.path = path #数据库连接参数  
+
+        if command!='':
+            conn=self.get_conn()
+            cu=conn.cursor()
+            cu.execute(command)
+
+    def get_conn(self):  
+        conn = sqlite3.connect(self.path)#,check_same_thread=False)  
+        conn.text_factory=str
+        return conn   
+
+    def conn_close(self,conn=None):  
+        conn.close()  
+
+    def conn_trans(func):  
+        def connection(self,*args,**kwargs):  
+            self.lock.acquire()  
+            conn = self.get_conn()  
+            kwargs['conn'] = conn  
+            rs = func(self,*args,**kwargs)  
+            self.conn_close(conn)  
+            self.lock.release()  
+            return rs  
+        return connection  
+
+
+    @conn_trans    
+    def execute(self,command,method_flag=0,conn=None):  
+        cu = conn.cursor()
+        try:
+            if not method_flag:
+                cu.execute(command)
+            else:
+                cu.execute(command[0],command[1])
+            conn.commit()
+        except sqlite3.IntegrityError,e:
+            print e
+            return -1
+        except Exception, e:
+            print e
+            return -2
+        return 0
+
+    @conn_trans
+    def fetchall(self,command="select name from xiaoqu",conn=None):
+        cu=conn.cursor()
+        lists=[]
+        try:
+            cu.execute(command)
+            lists=cu.fetchall()
+        except Exception,e:
+            print e
+            pass
+        return lists    
+
+
+def do_xiaoqu_spider(db_xq,region=u"昌平"):
+    """
+    爬取大区域中的所有小区信息
+    """
+    url=u"http://bj.lianjia.com/xiaoqu/rs"+region+"/"
+    try:
+        req = urllib2.Request(url,headers=hds[random.randint(0,len(hds)-1)])
+        source_code = urllib2.urlopen(req,timeout=5).read()
+        plain_text=unicode(source_code)#,errors='ignore')   
+        soup = BeautifulSoup(plain_text)
+    except (urllib2.HTTPError, urllib2.URLError), e:
+        print e
+        exit(-1)
+    except Exception,e:
+        print e
+        exit(-1)
+    d="d="+soup.find('div',{'class':'page-box house-lst-page-box'}).get('page-data')
+    #d={"totalPage":12,"curPage":1}
+    exec(d)
+    total_pages=d['totalPage']
+    print '%s : total_pages %d' % (region, total_pages)
+
+#    threads=[]
+#    for i in range(total_pages):
+#        url_page=u"http://bj.lianjia.com/xiaoqu/pg%drs%s/" % (i+1,region)
+#        t=threading.Thread(target=xiaoqu_spider,args=(db_xq,url_page))
+#        threads.append(t)
+#    for t in threads:
+#        t.start()
+#    for t in threads:
+#        t.join()
+
+def do_xiaoqu_chengjiao_spider(db_xq,db_cj):
+    """
+    批量爬取小区成交记录
+    """
+    count=0
+    xq_list=db_xq.fetchall()
+    for xq in xq_list:
+#        xiaoqu_chengjiao_spider(db_cj,xq[0])
+        count+=1
+        print 'have spidered %d xiaoqu' % count
+    print 'done'
+
+def exception_spider(db_cj):
+    """
+    重新爬取爬取异常的链接
+    """
+
+if __name__=="__main__":
+    command="create table if not exists xiaoqu (name TEXT primary key UNIQUE, regionb TEXT, regions TEXT, style TEXT, year TEXT)"
+    db_xq=SQLiteWraper('lianjia-xq.db',command)
+
+    command="create table if not exists chengjiao (href TEXT primary key UNIQUE, name TEXT, style TEXT, area TEXT, orientation TEXT, floor TEXT, year TEXT, sign_time TEXT, unit_price TEXT, total_price TEXT,fangchan_class TEXT, school TEXT, subway TEXT)"
+    db_cj=SQLiteWraper('lianjia-cj.db',command)
+
+    #爬下所有的小区信息
+    for region in regions:
+        do_xiaoqu_spider(db_xq,region)
+
+    #爬下所有小区里的成交信息
+    do_xiaoqu_chengjiao_spider(db_xq,db_cj)
+
+    #重新爬取爬取异常的链接
+    exception_spider(db_cj)
